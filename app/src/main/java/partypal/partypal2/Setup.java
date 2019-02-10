aa808ff7f7711473baf65ffadc78f39c80966e3f
bengw <bengw99@gmail.com>
2019-02-09 21:23:12 -0500

    Saving values works as text not as int

--- app/src/main/java/partypal/partypal2/Setup.java
+++ app/src/main/java/partypal/partypal2/Setup.java
@@ -1,13 +1,30 @@
 package partypal.partypal2;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class Setup extends AppCompatActivity {
+    EditText weightvalue;
+    Button weightbutton;
+    RadioButton malebutton;
+    RadioButton femalebutton;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String WEIGHT = "weight";
+    public static final String SEX = "sex";
+
+    String weight;
+    boolean sex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +34,68 @@ public class Setup extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        weightvalue = (EditText) findViewById(R.id.weightValue);
+        weightbutton = (Button) findViewById(R.id.weightButton);
+        malebutton = (RadioButton) findViewById(R.id.maleButton);
+        femalebutton = (RadioButton) findViewById(R.id.femaleButton);
+
+        weightbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveWeight();
+            }
+        });
+
+        malebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMale(true);
+            }
+        });
+
+        femalebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMale(false);
+            }
+        });
+
+        loadData();
+        updateVisible();
+    }
+
+    private void saveWeight() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(WEIGHT, weightvalue.getText().toString());
+        editor.apply();
+    }
+
+    private void saveMale(boolean val) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(SEX, val);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        String testeree = sharedPreferences.getString(WEIGHT, "932");
+        System.out.println("megamanmegaman   " + testeree + "    TESTETESTESTESTESTESTESTESTESTESTESTESTEST");
+        weight = testeree;
+        sex = sharedPreferences.getBoolean(SEX, true);
+    }
+
+    private void updateVisible() {
+        weightvalue.setText(weight);
+        if (sex) {
+            malebutton.setChecked(true);
+            femalebutton.setChecked(false);
+        } else {
+            malebutton.setChecked(false);
+            femalebutton.setChecked(true);
+        }
     }
 
 }
