255e4a88ee04fb723b8eb96aa9cbb109d7766a74
bengw <bengw99@gmail.com>
2019-02-10 00:11:23 -0500

    how to image

--- app/src/main/java/partypal/partypal2/MainActivity.java
+++ app/src/main/java/partypal/partypal2/MainActivity.java
@@ -3,15 +3,21 @@ package partypal.partypal2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView mainimg = (ImageView) findViewById(R.id.imageView3);  //HI TROY
+        int imageResource = getResources().getIdentifier("@drawable/shot", null, this.getPackageName());  //THIS TOO
+        mainimg.setImageResource(imageResource); //LEMONPARTY
 
         Button setup = (Button) findViewById(R.id.button2);
         setup.setOnClickListener(new View.OnClickListener() {
